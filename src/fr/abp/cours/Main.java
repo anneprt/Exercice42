4f3b8e3f29ae497fb3467dac814e9289fdbd9ce2
Anne <patesdegeek@gmail.com>
2020-04-16 00:23:52 +0200

    programme

--- src/fr/abp/cours/Main.java
+++ src/fr/abp/cours/Main.java
@@ -1,8 +1,18 @@
 package fr.abp.cours;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int valeur;
+        Scanner lectureClavier = new Scanner(System.in);
+        do {
+            System.out.println("Entrez une valeur");
+            valeur = lectureClavier.nextInt();
+
+        } while (valeur < 0 || valeur < 100);
+
+        System.out.println("Vous avez saisi la valeur " + valeur);
     }
 }
